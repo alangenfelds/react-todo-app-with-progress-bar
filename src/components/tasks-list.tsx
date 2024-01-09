63e3bf4149e9770cb65f3cec89e4fe192bccc3fc
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2024-01-09 16:09:26 +0200

    added selector

--- src/components/tasks-list.tsx
+++ src/components/tasks-list.tsx
@@ -2,6 +2,8 @@ import { TTodoItem } from "../types";
 import TodoItem from "./todo-item";
 import "./tasks-list.scss";
 import AddTodo from "./add-todo";
+import Select, { SelectOption } from "./select";
+import { useState } from "react";
 
 const TODOS: TTodoItem[] = [
   {
@@ -29,11 +31,17 @@ const TODOS: TTodoItem[] = [
 type Props = {};
 
 const TasksList = (props: Props) => {
+  const [selectedOption, setSelectedOption] = useState<SelectOption>("All");
+
+  const handleSelect = (selected: SelectOption) => {
+    setSelectedOption(selected);
+  };
+
   return (
     <div className="tasks-list-container">
       <div className="tasks-list-header">
         <div className="tasks-label">Tasks</div>
-        <div>Selector</div>
+        <Select selectedOption={selectedOption} onSelectChange={handleSelect} />
       </div>
       <div className="tasks-list">
         {TODOS.map((todo) => (
