3ce7a75c757453aa5d2c020fce88113508d075fb
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2024-01-09 19:24:31 +0200

    added custom checkbox for todo item

--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -1,4 +1,5 @@
 import { Todo } from "../../types";
+import CustomCheckbox from "../ui/custom-checkbox";
 import DotsMenu from "./dots";
 
 import "./todo-item.scss";
@@ -6,11 +7,11 @@ import "./todo-item.scss";
 type Props = { data: Todo };
 
 const TodoItem = ({ data }: Props) => {
-  const { title, completed } = data;
+  const { id, title, completed } = data;
   return (
     <div className="todo-item">
-      <div className="flex gap-10">
-        <div>check</div>
+      <div className="left-side">
+        <CustomCheckbox todoId={id} completed={completed} />
         <div className={`${completed ? "completed" : ""}`}>{title}</div>
       </div>
       <DotsMenu />
---
+++ src/components/ui/custom-checkbox.tsx
@@ -0,0 +1,29 @@
+import { useTodos } from "../../context/TodosContext";
+import { patchTodo } from "../../services/todosService";
+import CheckIcon from "./checkIcon.svg";
+
+import "./custom-checkbox.scss";
+
+type Props = {
+  todoId: number;
+  completed: boolean;
+};
+
+const CustomCheckbox = ({ todoId, completed }: Props) => {
+  const { refetchTodos } = useTodos();
+
+  const handleClick = () => {
+    patchTodo(todoId, { completed: !completed });
+    refetchTodos();
+  };
+  return (
+    <div
+      className={` custom-checkbox ${completed ? "task-completed" : ""}`}
+      onClick={handleClick}
+    >
+      {completed && <img src={CheckIcon} alt="check icon" />}
+    </div>
+  );
+};
+
+export default CustomCheckbox;
